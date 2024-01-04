3a16c8a0bdd6d64fc977f095bd9f2ebbbe7f146a
vishalprabhu2018 <prabhuvishal2018@gmail.com>
2024-01-04 08:44:10 +0530

    done with pagination

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -3,22 +3,24 @@ import {useState,useEffect} from 'react'
 import axios from 'axios'
 import Pokemon from '../Pokemon/Pokemon';
 import Loading from '../Loading/Loading';
-
+import './PokemonList.css'
 function PokemonList() {
 const [isLoading,setIsLoading]=useState(true)
 const [pokemonList,setPokemonList]=useState([])
-const pokedexUrl='https://pokeapi.co/api/v2/pokemon/';
+const [pokedexUrl,setPokedexUrl]=useState('https://pokeapi.co/api/v2/pokemon/');
+const [prevUrl, setPrevUrl]=useState('');
+const [nextUrl, setNextUrl]=useState('')
 
    async function downloadPokemon(){
+    setIsLoading(true)
         const response=await axios.get(pokedexUrl);
-        console.log(response.data.results);
         const results=response.data.results;
+        setNextUrl(response.data.next);
+        setPrevUrl(response.data.previous);
 
         const pokemonResultPromise=results.map(pokemon=>axios.get(pokemon.url));
-        console.log(pokemonResultPromise)
 
         const pokemonData=await axios.all(pokemonResultPromise);
-        console.log(pokemonData)
         const pokemonListResult=pokemonData.map((pokedata)=>{
         const pokemon=pokedata.data
             return{
@@ -41,14 +43,23 @@ const pokedexUrl='https://pokeapi.co/api/v2/pokemon/';
 
     useEffect(()=>{
         downloadPokemon();
-    })
+    },[pokedexUrl])
     return (
       <div>
-          <div className="grid  grid-cols-5  ml-24  gap-10">
+          <div className="grid  grid-cols-5  ml-24  gap-10 min-h-full">
               {(isLoading)?  <Loading/>: 
               pokemonList.map((el)=><Pokemon name={el.name} image={el.image} id={el.id}  key={el.id}    />)
               }
 
+          </div>
+
+          <div className="text-2xl  flex justify-center gap-10">
+            <button disabled={prevUrl==null}  onClick={()=>setPokedexUrl(prevUrl)} className="border rounded border-red-600 bg-red-600 cursor-pointer text-white px-4 py-1 mb-10 mt-8">Prev</button>
+            <button disabled={nextUrl==null}  onClick={()=>setPokedexUrl(nextUrl)} className="border rounded border-red-600 bg-red-600 cursor-pointer text-white px-4 py-1 mb-10 mt-8">Next</button>
+        
+          </div>
+          <div className='test'>
+            Vishal
           </div>
       </div>
     )
